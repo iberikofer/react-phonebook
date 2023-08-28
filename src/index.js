324b9f37f5e637f2e245b6808709d9f2ba1d8f79
Yaroslav <sych521@gmail.com>
2023-08-28 23:39:16 +0300

    final fix of The bug=)

--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ import { App } from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/Phonebook">
-          <App />
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<Provider store={store}>
+			<PersistGate loading={null} persistor={persistor}>
+				<BrowserRouter basename="/react-phonebook">
+					<App />
+				</BrowserRouter>
+			</PersistGate>
+		</Provider>
+	</React.StrictMode>
 );
